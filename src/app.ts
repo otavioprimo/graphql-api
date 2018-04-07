ba749b3b68b4ca578fea27c1a1f302bc31f0ee9d
Otávio Primo <otavioprimo@hotmail.com>
2018-04-07 17:54:49 -0300

    Implementação inicial do GraphQL

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
-let a: string = "haha";
-console.log(a);
+import * as express from 'express';
+import * as graphqlHttp from 'express-graphql';
+
+import schema from './graphql/schema';
+
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleware();
+    }
+
+    private middleware(): void {
+
+        this.express.use('/graphql', graphqlHttp({
+            schema: schema
+        }));
+
+    }
+}
+
+export default new App().express;

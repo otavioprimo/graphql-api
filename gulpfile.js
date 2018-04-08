c281332892815eb74adff3404d7a035928c46f11
Otavio Primo <otavioprimo@hotmail.com>
2018-04-07 23:23:30 -0300

    graphql schema e gulp com nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
 const ts = require('gulp-typescript');
+const livereload = require('gulp-livereload');
+const nodemon = require("gulp-nodemon");
 
 const tsProject = ts.createProject('tsconfig.json');
 
@@ -32,4 +34,18 @@ gulp.task('watch', ['build'], () => {
     return gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch'], () => {
+    // listen for changes
+    livereload.listen();
+    // configure nodemon
+    nodemon({
+        // the script to run the app
+        script: 'dist/index.js',
+        ext: 'js', 
+        env: { 'NODE_ENV': 'development' }
+    }).on('restart', function () {
+        // when the app has restarted, run livereload.
+        gulp.src('dist/index.js')
+            .pipe(livereload());
+    });
+});
--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,9 @@ class App {
     }
 
     private middleware(): void {
-
         this.express.use('/graphql', graphqlHttp({
-            schema: schema
+            schema: schema,
+            graphiql: process.env.NODE_ENV.trim() === 'development'
         }));
 
     }

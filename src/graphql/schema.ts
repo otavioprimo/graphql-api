7344ee8a6c76b5ca3b2f7974e656a8e8ab20dd56
Otávio Primo <otavioprimo@hotmail.com>
2018-04-10 12:03:55 -0300

    Post e Comment Schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -4,6 +4,8 @@ import { Query } from './query';
 import { Mutation } from './mutation';
 
 import { userTypes } from './resources/user/user.schema';
+import { postTypes } from './resources/post/post.schema';
+import { commentTypes } from './resources/comment/comment.schema';
 
 const SchemaDefinition = `
     type Schema {
@@ -17,6 +19,8 @@ export default makeExecutableSchema({
         SchemaDefinition,
         Query,
         Mutation,
+        commentTypes,
+        postTypes,
         userTypes
     ],
 });
